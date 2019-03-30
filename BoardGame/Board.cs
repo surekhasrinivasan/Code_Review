932c07f21c7613c59d87663d512f1b839f440d89
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-30 11:52:29 -0400

    Code review - initial commit

---
+++ BoardGame/Board.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGame
+{
+    class Board
+    {
+        private int _height;
+        private int _width;
+
+        public Board(int height, int width)
+        {
+            _height = height;
+            _width = width;
+
+            DrawBoard(height, width);
+        }
+
+        public void DrawBoard(int height, int width)
+        {
+            DrawTopBorder(width);
+
+            for (var i = 0; i < height; i++)
+                DrawCells(width);
+        }
+
+        public void DrawTopBorder(int width)
+        {
+            for (var i = 0; i < width; i++)
+                Console.Write("+--");
+            Console.WriteLine("+");
+        }
+
+        public void DrawCells(int width)
+        {
+            int j;
+
+            Console.Write("|  ");
+            for (j = 0; j < width; j++)
+                Console.Write("|  ");
+
+            if (j == width)
+            {
+                Console.WriteLine();
+                for (var i = 0; i < width; i++)
+                    Console.Write("+--");
+            }
+
+            Console.Write("+\n");
+        }
+    }
+}
--- BoardGame/Program.cs
+++ BoardGame/Program.cs
@@ -10,6 +10,26 @@ namespace BoardGame
     {
         static void Main(string[] args)
         {
+            Console.Write("Height of board: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Width of board: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Board board;
+
+            //var height = 4;
+            //var width = 6;
+
+            if (IsHeightAndWidthValid(height, width))
+                board = new Board(height, width);
+            else
+                Console.WriteLine("Height and width must be between 2 and 30.\n");
         }
+
+        public static bool IsHeightAndWidthValid(int height, int width)
+        {
+            return height > 2 && height <= 30 && width > 2 && width <= 30;
+        }    
     }
 }
