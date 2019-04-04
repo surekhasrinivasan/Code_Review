4035bcad3f831583057876f61a4b4918225cff71
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-04 11:43:22 -0400

    Created ConnectFourGame.cs

---
+++ V2-BoardGame/BoardGame/ConnectFourGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGame
+{
+    class ConnectFourGame
+    {
+        int player1;
+        int player2;
+        int[] redCheckers;
+        int[] yellowCheckers;
+        string[] moves;
+
+
+    }
+}
